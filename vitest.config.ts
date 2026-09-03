23cb05b384e0cfc6926d298aac4463eeeadba716
Ivan-Sher <vanyaneporojnev@gmail.com>
2026-09-03 14:14:31 +0300

    fix/replace @nuxt/test-utils with @vitejs/plugin-vue in vitest config second

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,16 @@
-import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'node:url'
 
-export default defineVitestConfig({
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '~': fileURLToPath(new URL('.', import.meta.url)),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
-    pool: 'threads',
   },
 })
